ecc2488d833717f52f2240289ebfceecaefd59ac
laura <laura_kenny85@hotmail.com>
2018-09-24 12:11:06 +0100

    added card and updated read me

--- src/pages/Page1.js
+++ src/pages/Page1.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { FormattedMessage } from 'react-intl';
 import RaisedButton from 'material-ui/RaisedButton';
+import {Card, CardHeader, CardMedia, CardTitle, CardText} from 'material-ui/Card';
 
 class Page1 extends Component {
 
@@ -11,10 +12,32 @@ class Page1 extends Component {
   render() {
     return (
       <div>
-        <h3>
-          <FormattedMessage id="Page1.header" defaultMessage="Hello, I am page 1." />
-        </h3>
-        <RaisedButton label={<FormattedMessage id="Page1.button" defaultMessage="Go to page 2" />} primary={true} onClick={this.handleClick.bind(this)} />
+        <div>
+          <h3>
+            <FormattedMessage id="Page1.header" defaultMessage="Hello, I am page 1." />
+          </h3>
+          <RaisedButton label={<FormattedMessage id="Page1.button" defaultMessage="Go to page 2" />} primary={true} onClick={this.handleClick.bind(this)} />
+        </div>
+        <div>
+          <Card style={{height: 500, width: 300, marginLeft: 200}}>
+          <CardHeader
+            title="URL Avatar"
+            subtitle="Subtitle"
+          />
+          <CardMedia
+            overlay={<CardTitle title="Overlay title" subtitle="Overlay subtitle" />}
+          >
+            <img src={require("../placeholder-image.png")} alt="" />
+          </CardMedia>
+          <CardTitle title="Card title" subtitle="Card subtitle" />
+          <CardText>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            Donec mattis pretium massa. Aliquam erat volutpat. Nulla facilisi.
+            Donec vulputate interdum sollicitudin. Nunc lacinia auctor quam sed pellentesque.
+            Aliquam dui mauris, mattis quis lacus id, pellentesque lobortis odio.
+          </CardText>
+          </Card>
+        </div>
       </div>
     );
   }
